ffe0407f2d71578e4dc740ce1e24a0995c2ef42c
Allen Bei <870970821@qq.com>
2024-05-11 14:57:15 +0800

    feat:- switch NEXTAUTH_URL

---
+++ .next/static/YA2x32JIfIIVc2stnYJJY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-33d7546d72ce5a10.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
